9b0e0895a5fe5af070ac15ee84284d1555f9ecbb
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2023-12-08 15:11:22 +0300

    fix merge issues

--- App.js
+++ App.js
@@ -4,17 +4,6 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 
 import Home from "./src/screens/Home";
-<<<<<<< HEAD
-import Signup from "./src/screens/Signup";
-import {Bill} from "./src/screens/Bill";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <Home /> */}
-      {/* <Signup /> */}
-      <Bill />
-=======
 import Signup from "./src/screens/Login";
 import Register from "./src/screens/Register";
 import AuthNavigator from "./src/navigation/AuthNavigator";
@@ -23,7 +12,6 @@ export default function App() {
   return (
     <NavigationContainer>
       {/* <SafeAreaProvider style={styles.container}> */}
->>>>>>> 18804089fda28223db8fa52cb724fa351c671a55
       <StatusBar style="auto" />
       <AuthNavigator />
       {/* </SafeAreaProvider> */}
@@ -40,4 +28,3 @@ const styles = StyleSheet.create({
     // padding: 10,
   },
 });
-
